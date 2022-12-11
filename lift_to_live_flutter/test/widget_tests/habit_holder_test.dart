c41ece722d0b41f10e776dcaf3783d18fb028be8
danielsevov <70882890+danielsevov@users.noreply.github.com>
2022-12-11 13:33:57 +0100

    Test habit holder widget

---
+++ lift_to_live_flutter/test/widget_tests/habit_holder_test.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lift_to_live_flutter/domain/entities/habit.dart';
+import 'package:lift_to_live_flutter/domain/entities/habit_task.dart';
+import 'package:lift_to_live_flutter/helper.dart';
+import 'package:lift_to_live_flutter/presentation/presenters/habits_page_presenter.dart';
+import 'package:lift_to_live_flutter/presentation/ui/widgets/habit_holder.dart';
+import 'package:lift_to_live_flutter/presentation/ui/widgets/habit_task_holder.dart';
+import 'package:mockito/annotations.dart';
+import 'habit_task_holder_test.mocks.dart';
+
+@GenerateMocks([HabitsPagePresenter])
+void main() {
+
+  testWidgets('HabitHolderWidget test constructor thumb up', (tester) async {
+    await tester.runAsync(() async {
+      // tests
+      final HabitsPagePresenter presenter = MockHabitsPagePresenter();
+      final HabitTask habitTask = HabitTask('This is the task', true);
+      final Habit habit = Habit(1, '946681200000', 'This is a note', 'A', 'A', false, [habitTask]);
+      final habitTaskWidget = HabitTaskHolder(habit: habit, habitTask: habitTask, presenter: presenter);
+      final widget = HabitHolder(habit: habit, habitTaskWidgets: [habitTaskWidget]);
+
+      final checkBoxFinder = find.byType(HabitTaskHolder);
+      final noteFinder = find.text('Coach note: This is a note');
+      final iconFinder = find.byIcon(Icons.thumb_up);
+
+      await tester.pumpWidget(MaterialApp(
+          title: 'Flutter Demo', home: Scaffold(body: Center(child: Container(color: Helper.blueColor,child: widget),))));
+
+      await tester.pump(const Duration(seconds: 2));
+      await tester.pumpAndSettle();
+
+      expect(checkBoxFinder, findsOneWidget);
+      expect(noteFinder, findsOneWidget);
+      expect(iconFinder, findsOneWidget);
+    });
+  });
+
+  testWidgets('HabitHolderWidget test constructor thumb down', (tester) async {
+    await tester.runAsync(() async {
+      // tests
+      final HabitsPagePresenter presenter = MockHabitsPagePresenter();
+      final HabitTask habitTask = HabitTask('This is the task', false);
+      final Habit habit = Habit(1, '946681200000', 'This is a note', 'A', 'A', false, [habitTask]);
+      final habitTaskWidget = HabitTaskHolder(habit: habit, habitTask: habitTask, presenter: presenter);
+      final widget = HabitHolder(habit: habit, habitTaskWidgets: [habitTaskWidget]);
+
+      final iconFinder = find.byIcon(Icons.thumb_down);
+
+      await tester.pumpWidget(MaterialApp(
+          title: 'Flutter Demo', home: Scaffold(body: Center(child: Container(color: Helper.blueColor,child: widget),))));
+
+      await tester.pump(const Duration(seconds: 2));
+      await tester.pumpAndSettle();
+
+      expect(iconFinder, findsOneWidget);
+    });
+  });
+
+  testWidgets('HabitHolderWidget test constructor thumb down', (tester) async {
+    await tester.runAsync(() async {
+      // tests
+      final HabitsPagePresenter presenter = MockHabitsPagePresenter();
+      final HabitTask habitTask = HabitTask('This is the task', false), habitTask2 = HabitTask('This is the task', true);
+      final Habit habit = Habit(1, '946681200000', 'This is a note', 'A', 'A', false, [habitTask, habitTask2]);
+      final habitTaskWidget = HabitTaskHolder(habit: habit, habitTask: habitTask, presenter: presenter);
+      final habitTaskWidget2 = HabitTaskHolder(habit: habit, habitTask: habitTask2, presenter: presenter);
+      final widget = HabitHolder(habit: habit, habitTaskWidgets: [habitTaskWidget, habitTaskWidget2]);
+
+      final iconFinder = find.byIcon(Icons.thumbs_up_down);
+
+      await tester.pumpWidget(MaterialApp(
+          title: 'Flutter Demo', home: Scaffold(body: Center(child: Container(color: Helper.blueColor,child: widget),))));
+
+      await tester.pump(const Duration(seconds: 2));
+      await tester.pumpAndSettle();
+
+      expect(iconFinder, findsOneWidget);
+    });
+  });
+}
